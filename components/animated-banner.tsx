e00b9cfbcc8501bc28a76cb5ba103660403646cb
jamessielaw-collab <jamessielaw@yahoo.com>
2025-11-01 21:15:05 +0100

    Update animated-banner.tsx

--- components/animated-banner.tsx
+++ components/animated-banner.tsx
@@ -1,20 +1,23 @@
 export default function AnimatedBanner() {
   return (
-    <section id="banner" className="bg-cream py-8 overflow-hidden border-y border-gray-200">
+    <section
+      id="banner"
+      className="bg-cream py-8 overflow-hidden border-y border-gray-200"
+    >
       <div className="relative">
         <div className="flex animate-marquee whitespace-nowrap">
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>NATURAL BEAUTY</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>TIMELESS ELEGANCE</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>BRIDAL</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>NATURAL BEAUTY</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>TIMELESS ELEGANCE</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>BRIDAL</span>
-          <span className="text-4xl text-black mx-8" style={{ fontFamily: 'MADE Mirage, sans-serif', fontWeight: 'normal' }}>•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">NATURAL BEAUTY</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">TIMELESS ELEGANCE</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">BRIDAL</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">NATURAL BEAUTY</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">TIMELESS ELEGANCE</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">BRIDAL</span>
+          <span className="font-playfair text-4xl lg:text-5xl text-black mx-8">•</span>
         </div>
       </div>
     </section>
